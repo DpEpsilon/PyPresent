0c3aff5aa17e1d23357347bc794d33364a933df4
ABYSS <theabyscries@gmail.com>
2013-02-27 20:26:32 +1100

    Added TextSlide class

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ BACKGROUND_COLOUR = (0, 0, 0)
 
 # Modules will assume that pygame has been initialised, therefore they are imported after pygame.init()
 pygame.init()
-import wrap 
+import slide
 
 pygame.display.set_mode((WIDTH, HEIGHT))
 screen = pygame.display.get_surface()
 screen.fill(BACKGROUND_COLOUR)
 
-wrap.render_text(screen, "dkn knn n skn dknf knskjfnkj k nkn kjdsnfkjnk kn ksnk ", TEXT_COLOR, BACKGROUND_COLOUR, 1)
+s = slide.TextSlide([slide.TextBox(20,20,150,200,"I like muffins and cheesecacke emacs is the best"), slide.TextBox(20, 50, 150, 200, "LOLOL overlap lol hs1917")], \
+	[],BACKGROUND_COLOUR)
+s.draw_slide(screen)
 pygame.display.flip()
 
 while True:
--- slide.py
+++ slide.py
@@ -2,17 +2,29 @@ import pygame
 from pygame.locals import *
 import wrap
 
-default_font = pygame.font.SysFont("arial", 30)
+default_font = pygame.font.SysFont("arial", 15)
 default_colour = (255, 255, 255)
 
 class TextBox():
 	def __init__(self, x, y, width, height, text, font=default_font, font_colour=default_colour):
 		self.x = x; self.y = y
 		self.width = width; self.height = height
-		self.font = font; self.font_size = self.font_size; self.font_colour = font_colour
+		self.font = font; self.font_colour = font_colour
 		self.text = text
 		self.surf = None
+		#Note: Rendering in init for now
+		self.render()
 
 	def render(self):
 		self.surf = pygame.Surface((self.width, self.height))
 		wrap.render_text(self.surf, self.text, self.font, self.font_colour)
+
+class TextSlide():
+	def __init__(self, text_boxes, images, back_colour):
+		self.text_boxes = text_boxes
+		self.images = images
+		self.back_colour = back_colour
+
+	def draw_slide(self, screen_surf):
+		for text_box in self.text_boxes:
+			screen_surf.blit(text_box.surf, (text_box.x, text_box.y))
